380a632f999b3a7113838582b925456e4bb95472
LeeKyuchan <time2209@naver.com>
2021-09-02 12:32:20 +0900

    post 방식 처리 예시

--- ExpressExample/app7.js
+++ ExpressExample/app7.js
@@ -3,17 +3,23 @@ var http = require('http');
 var static = require('serve-static');
 var path = require('path');
 
+//post 방식으로 보내기 위한 모듈
+var bodyParser = require('body-parser');
+
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
 app.use('/public', static(path.join(__dirname, 'public')));
 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
 app.use(function (req, res, next) {
     console.log('첫번째 미들웨어 호출됨.');
 
     var userAgent = req.header('User-Agent');
-    var paramName = req.query.name;
+    var paramName = req.body.name || req.query.name;
 
     res.send('<h3>서버에서 응답, userAgent: ' + userAgent + ' </h3><h3>서버에서 응답, paramName: ' + paramName +' </h3>');
 });
