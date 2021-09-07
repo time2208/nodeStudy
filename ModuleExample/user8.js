7a747969cbb72a91e02cd9a6e683248c8ad9d8d8
LeeKyuchan <time2209@naver.com>
2021-09-07 14:31:52 +0900

    프로토타잊 객체를 할당

---
+++ ModuleExample/user8.js
@@ -0,0 +1,17 @@
+//프로토타입 객체를 할당
+function User(id, name){
+    this.id = id;
+    this.name = name;
+}
+
+User.prototype.getUser = function(){
+    return {id:this.id, name: this.name};
+};
+
+User.prototype.group = {id:'그룹01', name:'친구'};
+
+User.prototype.printUser = function(){
+    console.log('user 이름: ' + this.name + ', group: ' + this.group.name );
+};
+
+module.exports = new User('test01', '찬');
