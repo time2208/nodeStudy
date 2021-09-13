a2f95bb7ac2b3b8209158c059860c188cdc3d190
LeeKyuchan <time2209@naver.com>
2021-09-13 12:46:50 +0900

    passport 모듈화

---
+++ PassportExample2/app.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var http = require('http');
+var static = require('serve-static');
+var path = require('path');
+
+//post 방식으로 보내기 위한 모듈
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+
+//에러 핸들러 모듈 사용
+var expressErrorHandler = require('express-error-handler');
+
+//user 모듈 불러오기
+var user = require('./routes/user');
+
+var config = require('./config/config');
+
+var database_loader = require('./database/database_loader');
+var route_loader = require('./routes/route_loader');
+
+//암호화 모듈
+var crypto = require('crypto');
+
+//===== passport 사용 =====//
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var app = express();
+
+app.set('views', __dirname+'/views');
+app.set('view engine', 'ejs');
+//app.set('view engine', 'pug');
+
+console.log('config.server_port -> ' + config.servrt_port);
+app.set('port', config.servrt_port || 3000);
+
+app.use('/public', static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+app.use(expressSession({
+    secret: 'my key',
+    resave: true,
+    saveUninitialized: true
+}));
+
+
+//===== Passport 초기화 =====//
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+var configPassport = require('./config/passport');
+configPassport(app, passport());
+
+
+var router = express.Router();
+route_loader.init(app, router);
+
+var userPassport = require('./routes/user_passport');
+userPassport(router, passport);
+
+
+
+
+//404에러 페이지 처리
+var errorHandler = expressErrorHandler({
+    static: {
+        '404': './public/404.html'
+    }
+});
+
+app.use(expressErrorHandler.httpError(404));
+app.use(errorHandler);
+
+var server = http.createServer(app).listen(app.get('port'), function () {
+    console.log('익스프레스로 웹 서버를 실행함 :' + app.get('port'));
+    
+    database_loader.init(app, config);
+});
